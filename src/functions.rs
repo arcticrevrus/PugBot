1a3cd2cef8f16b3968407a94d169ac860f65360d
ArcticRevrus <arcticrevrus@gmail.com>
2024-01-01 11:19:27 -0600

    Create interaction_create handler function

--- src/functions.rs
+++ src/functions.rs
@@ -3,12 +3,12 @@ use serenity::{prelude::*, all::{*}};
 use crate::handler::*;
 use tokio::sync::RwLockWriteGuard;
 
-pub fn check_first_launch(mut data: RwLockWriteGuard<'_, Data>) -> bool {
-    if data.first_launch {
+pub fn check_first_launch(mut data: RwLockWriteGuard<'_, Data>) -> (bool, RwLockWriteGuard<'_, Data>) {
+    let first_launch = data.first_launch;
+    if first_launch {
         data.first_launch = false;
-        return true
     }
-    return false
+    (first_launch, data)
 }
 
 pub async fn initialize_data(ctx: &Context) -> Arc<RwLock<Data>> {
@@ -35,4 +35,45 @@ pub async fn get_listen_channel(ctx: &Context, data: &Data) -> ChannelId {
         }
     }
     panic!("get_listen_channel failed to return channel")
+}
+
+pub async fn clean_messages(ctx: &Context, channel: &Channel, user: &UserId) {
+    for message in channel.id().messages(&ctx, GetMessages::new()).await.unwrap() {
+        if &message.author.id == user {
+            if message.content.contains("The current queue") {
+                message.delete(&ctx).await.unwrap();
+            }
+        }
+    }
+}
+
+pub fn create_message_contents() -> CreateMessage {
+    let message = 
+">>> The current queue is: 
+<:tank:444634700523241512> : blah
+<:heal:444634700363857921> : blah
+<:dps:444634700531630094> :  blah";
+    let buttons = make_buttons();
+    let mut contents = CreateMessage::new().content(message);
+
+    for button in &buttons {
+        contents = contents.button(button.clone())
+    }
+    return contents
+}
+
+pub fn make_buttons() -> Vec<CreateButton> {
+    let tank_button = CreateButton::new("add_tank")
+    .label("Tank")
+    .style(ButtonStyle::Primary);
+let healer_button = CreateButton::new("add healer")
+    .label("Healer")
+    .style(ButtonStyle::Success);
+let dps_button = CreateButton::new("add_dps")
+    .label("DPS")
+    .style(ButtonStyle::Danger);
+let leave_button = CreateButton::new("leave")
+    .label("Leave")
+    .style(ButtonStyle::Secondary);
+    vec![tank_button, healer_button, dps_button, leave_button]
 }
--- src/handler.rs
+++ src/handler.rs
@@ -1,6 +1,7 @@
 
 use std::sync::Arc;
-use serenity::all::Channel;
+use serenity::all::Interaction;
+use serenity::builder::CreateInteractionResponse;
 use serenity::prelude::*;
 use serenity::async_trait;
 use serenity::model::channel::Message;
@@ -38,17 +39,42 @@ pub struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
+        let bot_user_id = {
+            let current_user = ctx.cache.current_user();
+            current_user.id
+        };
         let data = initialize_data(&ctx).await;
-        let mut data = data.write().await;
+        let data = data.write().await;
         let channel = msg.channel(&ctx).await.expect("Failed to get channel");
+        if msg.author.id != bot_user_id {
+            clean_messages(&ctx, &channel, &bot_user_id).await;
+            if channel.id().name(&ctx).await.unwrap() == get_listen_channel(&ctx, &data).await.name(&ctx).await.unwrap() {
+                let contents = create_message_contents();
+                channel.id().send_message(&ctx, contents).await.unwrap();
+            }
+        }
+    }
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        match interaction.id() {
+            add_tank => {
+                println!("tank");
+            }
+            add_healer => println!("healer"),
+            add_dps => println!("dps"),
+            leave => println!("leave"),
+            _ => println!("not implemented")
+        }
+        
     }
     async fn ready(&self, ctx: Context, _: Ready) {
         let data = initialize_data(&ctx).await;
-        let mut data = data.write().await;
-        if check_first_launch(data) {
+        let data = data.write().await;
+        let (first_launch, data) = check_first_launch(data);
+
+        if first_launch {
             let channel = get_listen_channel(&ctx, &data).await;
             channel.say(&ctx.http, "Bot reloaded").await.expect("Failed to send message.");
         }
+        println!("Connected");
     }
-    println!("Connected");
 }
